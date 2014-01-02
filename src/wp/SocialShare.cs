726d26ea8551f0ebea01572a25db7ec4444624a9
Mayur <mayur.loved@gmail.com>
2014-01-02 12:45:38 +0530

    Socail Share For Window PhoneGap-By Mayur Panchal

---
+++ src/wp/SocialShare.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Runtime.Serialization;
+using Microsoft.Phone.Tasks;
+using WPCordovaClassLib.Cordova.JSON;
+
+namespace WPCordovaClassLib.Cordova.Commands
+{
+
+    public class SocialShare : BaseCommand
+    {
+        public enum ShareType
+        {
+            Status = 0, // status update to twitter/facebook/... all supported device accounts
+            Link        // share a link on twitter/facebook/ ... all supported device accounts
+        }
+
+
+        [DataContract]
+        public class ShareOptions
+        {
+            [DataMember]
+            public string url;
+
+            [DataMember]
+            public string title;
+
+            [DataMember]
+            public string message;
+
+            [DataMember]
+            public ShareType shareType = ShareType.Status; // default
+        }
+
+        public void share(string options)
+        {
+            string[] args = JsonHelper.Deserialize<string[]>(options);
+            string callbackId = args[1];
+
+            ShareOptions opts = JsonHelper.Deserialize<ShareOptions>(args[0]);
+            switch (opts.shareType)
+            {
+                case ShareType.Status :
+                    shareStatus(opts.message);
+                    break;
+                case ShareType.Link :
+                    shareLink(opts.title, opts.url, opts.message);
+                    break;
+            }
+
+            DispatchCommandResult(new PluginResult(PluginResult.Status.OK), callbackId);
+        }
+
+        protected void shareStatus(string msg)
+        {
+            ShareStatusTask shareStatusTask = new ShareStatusTask();
+            shareStatusTask.Status = msg;
+            shareStatusTask.Show();
+        }
+
+        protected void shareLink(string title, string url, string msg)
+        {
+            ShareLinkTask shareLinkTask = new ShareLinkTask();
+            shareLinkTask.Title = title;
+            shareLinkTask.LinkUri = new Uri(url, UriKind.Absolute);
+            shareLinkTask.Message = msg;
+            shareLinkTask.Show();
+        }
+    }
+}
---
+++ www/socialshare.js
@@ -0,0 +1,17 @@
+﻿
+// (c) 2011-2013 Jesse MacFadyen,  Adobe Systems Incorporated
+
+var exec = require('cordova/exec');
+
+module.exports = {
+
+    shareStatus: function (msg) {
+        var options = { "message": msg, "shareType": 0 }; // 0 == status
+        exec(null, null, "SocialShare", "share", [options]);
+    },
+
+    shareLink: function (title, url, msg) {
+        var options = { "message": msg, "title": title, "url": url, "shareType": 1 }; // 1 == link
+        exec(null, null, "SocialShare", "share", [options]);
+    }
+}
